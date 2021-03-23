31fe6cdcf0045030dd9bb16f04f650c98a8c8709
Farhana <farhanafatema@hotmail.com>
2021-03-23 12:28:19 +0000

    working on homepage svg

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+// import Navbar from 'react-bootstrap/Navbar';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
@@ -15,18 +15,20 @@ function HomePage(){
     return(
         
         <Container fluid>
-         <Navbar className="mb-0" bg="dark" variant="dark" expand="lg">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 250"><path fill="#0099ff" fillOpacity="1" d="M0,288L60,261.3C120,235,240,181,360,165.3C480,149,600,171,720,170.7C840,171,960,149,1080,133.3C1200,117,1320,107,1380,101.3L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
+         {/* <Navbar className="mb-0" bg="dark" variant="dark" expand="lg">
             <Navbar.Brand style={ {color:"white" } }> Access4All</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">            
             </Navbar.Collapse>
-        </Navbar>
+        </Navbar> */}
+        <h1>HOME PAGE</h1>
         <Row>
             <Col>
             <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src= {videoIcon} />
             <Card.Body>
-            <Button variant="primary" style={{"margin-left": '4rem'}}>Video Call</Button>
+            <Button variant="primary" style={{"marginLeft": '4rem'}}>Video Call</Button>
              </Card.Body>
             </Card>
             </Col>
@@ -36,7 +38,7 @@ function HomePage(){
             <Card.Img variant="top" src={keyboardIcon} />
             <Card.Body>
             
-            <Button variant="primary" style={{"margin-left": '4rem'}}>Text Chat</Button>
+            <Button variant="primary" style={{"marginLeft": '4rem'}}>Text Chat</Button>
              </Card.Body>
             </Card>
             </Col>
@@ -67,6 +69,7 @@ function HomePage(){
             </Card>
             </Col>
         </Row>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fillOpacity="1" d="M0,192L80,213.3C160,235,320,277,480,266.7C640,256,800,192,960,149.3C1120,107,1280,85,1360,74.7L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
     </Container>
         
     );
