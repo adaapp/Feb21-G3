de169aebb18463bc7d02a2ebde58e9c399bebdc4
Farhana <farhanafatema@hotmail.com>
2021-03-23 15:19:39 +0000

    homepage completed

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,10 +7,10 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import './App.css';
-import videoIcon from './videoIcon.png';
-import keyboardIcon from './keyboardIcon.jpg';
-import gamesIcon from './gameIcon.png';
-import helpIcon from './help.png';
+import videoIcon from './assets/videoIcon.png';
+import keyboardIcon from './assets/keyboardIcon.jpg';
+import gamesIcon from './assets/gameIcon.png';
+import helpIcon from './assets/help.png';
 
 
 
@@ -18,11 +18,6 @@ function HomePage(){
     return(
         
         <Container fluid>
-       
-        {/* <div className = "top-curve">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 275" style ={{width: '50%'}}><path fill="#0099ff" fillOpacity="1" d="M0,288L60,277.3C120,267,240,245,360,213.3C480,181,600,139,720,101.3C840,64,960,32,1080,16C1200,0,1320,0,1380,0L1440,0L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
-        <h1>HOME PAGE</h1>
-        </div> */}
         <TopCurve heading = 'Home Page'/>
         
         <Row style={{"marginTop": '4rem'}}>
