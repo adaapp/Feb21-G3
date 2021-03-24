5d3ff07a82bebb4a519c135c69769a764684f1d4
Ahmad <yahmad@uk-fvfc21lyl414.home>
2021-03-24 09:32:53 +0000

    game page card creation

--- src/Games.js
+++ src/Games.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+// import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import TopCurve from './TopCurve.js'
 import BottomCurve from './BottomCurve.js'
 
 
+import pongIcon from './assets/pong.png';
+import equipmentIcon from './assets/equipment.png';
+
+
 function Games(){
     return(
-        <>
+        <Container fluid>
         <TopCurve heading='Games'/>
+        <Row>
+            <Col>
+            <Card style={{ width: '18rem'}} style={{"marginLeft": '21rem'}}>
+            <Card.Img variant="top" src={pongIcon}/>
+            <Card.Body>
+            <Button variant="primary" style={{"marginLeft": '5rem'}} >Pong</Button>
+             </Card.Body>
+            </Card>
+            </Col>
+
+            <Col>
+            <Card style={{ width: '18rem'}} style={{"marginRight": '21rem'}}>
+            <Card.Img variant="top" src={equipmentIcon}/>
+            <Card.Body>
+            <Button variant="primary" style={{"marginLeft": '4rem'}}  >to be defined</Button>
+             </Card.Body>
+            </Card>
+            </Col>
+        </Row>
         <BottomCurve/>
-        </>
+    </Container>
     );
 }
 
