680d9bac414566bb2fb34b5b531e15d28050e3aa
Ahmad <yahmad@uk-fvfc21lyl414.home>
2021-03-25 17:56:03 +0000

    bug fixes

--- src/CallDad.js
+++ src/CallDad.js
@@ -42,13 +42,8 @@ function VideoCall(){
             </Col>
             <Col xs={6} className="video-second-col"> 
             <div className= "call-contact">     
-<<<<<<< HEAD
-            <Image width="700" className="rounded mx-auto d-block" style={{"marginTop": '7rem'}} src={dadFull} />
-            <Image width="500"  className="rounded mx-auto d-block" style={{"marginTop": '10rem'}} src={onButtons}onClick={navigateToVideoCall}/>
-=======
             <Image width="900" style={{"marginLeft":'2.9rem',"marginTop": '3rem'}} src={dadFull} />
             <Image width="700"  style={{"marginLeft":'10rem',"marginTop": '10rem'}} src={onButtons} onClick={navigateToVideoCall}/>
->>>>>>> login-page
                        
             </div>
             </Col>
